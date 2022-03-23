ffc9e4a85d3ed4f78748cfc1a6eb9e7215c3b4f7
Ghassanooooo <ghassanaldarwish@gmail.com>
2022-03-23 10:50:35 +0100

    exercises templates

--- Classes/1-intro.js
+++ Classes/1-intro.js
@@ -1,1 +1,9 @@
-console.log('test')
+// * ES6 CLASS INTRODUCTION
+
+// The concept of "class" in JavaScript was introduced in ES6.
+// Internally, a class is a "wrapper" over the normal way JavaScript works.
+
+// A class still gives you an efficient way of creating "types" of object, and "giving" them default properties and methods.
+
+// Class syntax is more comparable to similar features available in C#, Java etc.
+
