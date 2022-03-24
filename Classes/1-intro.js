2645e069fcde69fb4cc98e3b6a73df4a706de8a7
Ghassanooooo <ghassanaldarwish@gmail.com>
2022-03-24 10:19:37 +0100

    classes intro

--- Classes/1-intro.js
+++ Classes/1-intro.js
@@ -7,3 +7,58 @@
 
 // Class syntax is more comparable to similar features available in C#, Java etc.
 
+//---------------//
+// Passing params to functions in (param1, param2,...)
+/*function personFn(name, email) {
+    console.log(name)
+    console.log(email)
+}
+personFn('Pilar', 'pilar@gmail.com')*/
+
+//-------------//
+// let email (3)
+
+class Person{
+    constructor(personName, personEmail) {
+        this.name = personName;
+        this.email = personEmail
+    }
+    greeting() {
+        return `Hallo ${this.name}`
+    }
+    test(email) {
+       // test(email) (2)
+       // let email (1)
+        console.log(email)
+    }
+}
+
+let personInit_1 = new Person('Pilar', 'pilar@gmail.com')
+// props
+console.log(personInit_1.name) // Pilar
+console.log(personInit_1.email) // pilar@gmail.com
+// method
+console.log(personInit_1.greeting()) // Hallo Pilar!
+
+// console.log(personInit_1.test()) // email is not defined
+
+// console.log( Person('Pilar', 'pilar@gmail.com')) // TypeError: Class constructor Person cannot be invoked without 'new'
+//------------
+let personInit_2 = new Person('Max', 'max@gmail.com')
+// props
+console.log(personInit_2.name) // Max
+console.log(personInit_2.email) // max@gmail.com
+// method
+console.log(personInit_2.greeting()) // Hallo Max
+//--------
+
+console.log(new Person('Pilar', 'pilar@gmail.com')) // Person { name: 'Pilar', email: 'pilar@gmail.com' }
+
+
+
+
+
+
+
+
+
